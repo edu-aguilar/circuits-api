94e3179c6ad898a3d280b8561b13bf0ffcd280f0
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-04-06 19:33:23 +0200

    fix tests

--- src/circuits/circuits.controller.spec.ts
+++ src/circuits/circuits.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CircuitsController } from './circuits.controller';
 import { CircuitsService } from './circuits.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Circuit } from './entities/circuit.entity';
 
 describe('CircuitsController', () => {
   let controller: CircuitsController;
@@ -8,7 +10,10 @@ describe('CircuitsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CircuitsController],
-      providers: [CircuitsService],
+      providers: [
+        CircuitsService,
+        { provide: getModelToken(Circuit.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     controller = module.get<CircuitsController>(CircuitsController);
--- src/circuits/circuits.service.spec.ts
+++ src/circuits/circuits.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CircuitsService } from './circuits.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Circuit } from './entities/circuit.entity';
 
 describe('CircuitsService', () => {
   let service: CircuitsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CircuitsService],
+      providers: [
+        CircuitsService,
+        { provide: getModelToken(Circuit.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<CircuitsService>(CircuitsService);
