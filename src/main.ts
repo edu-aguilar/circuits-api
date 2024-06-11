53504936122baa527fc597929da1fbfc9b7b183f
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2024-06-11 08:53:33 +0200

    chore: update main module to export app

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-let app;
-
-NestFactory.create(AppModule).then((_app) => {
-  app = _app;
+export default async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
-  app.listen(process.env.PORT || 3000);
-});
+  await app.listen(process.env.PORT || 3000);
+}
 
-export default app;
+bootstrap();
